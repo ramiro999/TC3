a4d654de570e3362ff491b97751b05117b0e4f54
DESKTOP-R2HQMPA\DELL <ramiroavila999@gmail.com>
2022-09-06 07:48:26 -0500

    trabajo en clase

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'categorias.apps.CategoriasConfig',
     'autores.apps.AutoresConfig',
     'libros.apps.LibrosConfig'
